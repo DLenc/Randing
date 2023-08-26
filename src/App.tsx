760cdda2fc02554b92c622ca76eb32afbd8a5670
alreadynyeong <kjw87130@naver.com>
2023-08-26 15:49:09 +0900

    #2 setting: 전체 max 너비 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Test from './page/youTube/test';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Test />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  max-width: 1688.05px;
+  margin: 0 auto;
+`;
 
 export default App;
