9d2aaa14d70ff734a7a23476cefa69d830d0093d
alreadynyeong <kjw87130@naver.com>
2023-09-02 17:34:38 +0900

    #6 feat: 폰트 추가 및 수정

--- src/style/font.ts
+++ src/style/font.ts
@@ -1,49 +1,73 @@
 import { CSSProperties } from 'react';
 import Color from './color';
 
+const Head1: CSSProperties = {
+  color: 'white',
+  fontSize: '63.302px',
+  fontWeight: 600,
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
+};
+
+const Head2: CSSProperties = {
+  color: 'white',
+  fontSize: '63.302px',
+  fontWeight: 400,
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
+};
+
 const Title1: CSSProperties = {
   color: Color.dlNavy,
   fontSize: '42.201px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const Title2: CSSProperties = {
   color: Color.dlNavy,
   fontSize: '49.235px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const Title3: CSSProperties = {
   color: Color.dlNavy,
   fontSize: '36.5px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const Sub: CSSProperties = {
   color: Color.subText,
   fontSize: '31.651px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const Content: CSSProperties = {
   color: Color.subText,
   fontSize: '24px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const SubContent: CSSProperties = {
   color: 'white',
   fontSize: '24px',
   fontWeight: 500,
-  lineHeight: 'normal'
+  lineHeight: 'normal',
+  fontFamily: 'Pretendard'
 };
 
 const FONT = {
+  Head1,
+  Head2,
   Title1,
   Title2,
   Title3,
