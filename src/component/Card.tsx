9f49bf006921c9375187a507fc16cc2f193d7002
marchfirst01 <140829640+marchfirst01@users.noreply.github.com>
2023-09-04 15:39:22 +0900

    Dev 개발 완료, 테스트 배포 브랜치로 병합 (#21)
    * Feature/#5 card detail (#20)
    
    * #5 feat: card content 안에 bold체 처리
    
    * #5 feat:card4 수정
    
    * Feature/#6 banner3 (#16)
    
    * #6 feat: font 파일 추가
    
    * #6 feat: banner3 구현
    
    * #6 fix: 폰트 수정
    
    ---------
    
    Co-authored-by: minhyeong <78204289+alreadynyeong@users.noreply.github.com>

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
+import { ReactComponent as Arrow } from '../assets/icon/Arrow.svg';
 import Color from '../style/color';
 import FONT from '../style/font';
-import { ReactComponent as Arrow } from '../assets/icon/Arrow.svg';
 
 interface CardProps {
   index?: string;
@@ -14,6 +14,7 @@ const Card = (props: CardProps) => {
   const textColor = props.index === '01' ? 'white' : Color.subText;
 
   const titles = props.title?.split('<br/>');
+  const content = props.content as string;
 
   return (
     <Container>
@@ -24,7 +25,9 @@ const Card = (props: CardProps) => {
             {titles?.map((title, index) => <div key={index}>{title}</div>)}
           </div>
         </Index>
-        <Content style={{ ...FONT.Content, color: textColor }}>
+        <Content 
+          dangerouslySetInnerHTML={{ __html: content }}
+          style={{ ...FONT.Content, color: textColor }}>
           {props.content}
         </Content>
         <Arrow stroke={textColor} />
