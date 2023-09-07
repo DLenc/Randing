f62a9a4716787caa5a539a3eba5dfc3822abb4df
alreadynyeong <kjw87130@naver.com>
2023-09-07 12:44:58 +0900

    #6 fix: 커버 로고 이미지로 변경(svg오류 문제)

--- src/page/cover.tsx
+++ src/page/cover.tsx
@@ -4,20 +4,17 @@ import FONT from '../style/font';
 
 import Triangle from '../component/triangle';
 
-// import { ReactComponent as Xicon } from '../../assets/icon/Xicon.svg';
-// import { ReactComponent as SoftSquared } from '../../assets/logo/SoftSquared.svg';
-// import { ReactComponent as DLenc } from '../../assets/logo/DLenc.svg';
-import { ReactComponent as Logo } from '../assets/logo/Logo.svg';
-
 const Cover = () => {
   return (
     <>
       <Container>
         <Icon>
-          <Logo />
-          {/* <DLenc />
-          <Xicon />
-          <SoftSquared /> */}
+          <img
+            src={require('../assets/logo/Logo.png')}
+            alt=''
+            width={487}
+            height={84.3}
+          />
         </Icon>
         <Title1 style={FONT.Head1}>층간소음 Solution,</Title1>
         <Title2 style={FONT.Head2}>D-Silent Floor & D-Silence Service</Title2>
