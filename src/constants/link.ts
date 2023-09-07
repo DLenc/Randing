6ee37017e937a53a7ffa83edfb9c2fc502443467
alreadynyeong <kjw87130@naver.com>
2023-09-07 12:59:56 +0900

    #22 feat: Footer 구현

---
+++ src/constants/link.ts
@@ -0,0 +1,15 @@
+interface LinkProps {
+  title: string;
+  link: string;
+}
+
+const Links: LinkProps[] = [
+  {
+    title: 'Youtube',
+    link: 'https://www.youtube.com/c/DL%EB%94%94%EC%97%98/featured'
+  },
+  { title: 'Blog', link: 'https://blog.naver.com/dlcommunication' },
+  { title: 'NaverTV', link: 'https://tv.naver.com/dlenc' }
+];
+
+export default Links;
